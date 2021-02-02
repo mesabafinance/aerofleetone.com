c315a9c5d712178e227025a1bcdfea9da1cb68d9
smnwltr <smn@wltr.co>
2021-02-01 18:25:49 -0700

    Implement basic mapbox plugin, split up tet on team site, change calendly widget to destiny

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -2,41 +2,6 @@
 // Insert year for copyright
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 
-$('.light-img').click(function (e) {
-    e.preventDefault();
-    $(this).ekkoLightbox();
-});
-
-
-
-// Add white background to navbar upon scroll
-
-$(document).ready(function () {
-    //caches a jQuery object containing the header element
-    var header = $(".navbar");
-    var links = $(".nav-item");
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-
-        if (scroll >= 250) {
-            header.addClass("navbar-scroll");
-        } else {
-            header.removeClass("navbar-scroll")
-        }
-    });
-});
-
-// smooth scrolling
-$(document).ready(function () {
-    $(".smooth-scroll").click(function (e) {
-        e.preventDefault();
-        var position = $($(this).attr("href")).offset().top;
-        $("html, body").animate({
-            scrollTop: position
-        }, 1000);
-    });
-});
-
 // Hamburger menu toggling
 window.onload = function () {
     // Look for .hamburger
@@ -56,50 +21,3 @@ window.onload = function () {
     });
 }
 
-// read the url to check for contact=sent parameter (contact form)
-$(document).ready(
-    function () {
-        let searchParams = new URLSearchParams(window.location.search);
-        let param = searchParams.get('contact');
-        if (param == 'sent') {
-            $('#contactForm').addClass('d-none');
-            $('#contactSuccess').removeClass('d-none');
-            $('#contactSuccess').addClass('d-block');
-            $('#contactSuccess').get(0).scrollIntoView();
-        }
-    }
-)
-
-// flip card once element is scrolled into view
-var mq = window.matchMedia("(max-width: 768px)");
-
-
-var scrollEventHandler = function () {
-    if (isScrolledIntoView(document.getElementById('firstflip')) && mq.matches) {
-        $("#firstflip").addClass("hovered");
-        unbindScrollEventHandler();
-    }
-}
-
-function unbindScrollEventHandler() {
-    $(document).unbind('scroll', scrollEventHandler);
-}
-
-$(document).scroll(scrollEventHandler);
-
-function isScrolledIntoView(el) {
-    var elemTop = el.getBoundingClientRect().top;
-    var elemBottom = el.getBoundingClientRect().bottom;
-
-    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    return isVisible;
-}
-
-// make flip cards clickable on mobile
-
-if (mq.matches) {
-    $(".fliplink").click(function () {
-        $(this).children().toggleClass("hovered");
-        $(".fliplink").children().removeClass("hovered");
-    });
-}
