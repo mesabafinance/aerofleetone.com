a85755d6c5c45aa225dc84904a90fdb5ee07f5e1
smnwltr <smn@wltr.co>
2021-09-20 15:46:19 -0600

    Improve blog section layout on index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,20 @@
 // Insert year for copyright
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+// handle cookie message display
+
+function setMessageDismissed() {
+    localStorage.setItem('cookieMsg', 'dismissed');
+}
+
+let msgCheck = localStorage.getItem('cookieMsg');
+
+// if (msgCheck != 'dismissed') {
+//     $('#cookie-msg').slideDown(1000);
+// }
+
+$('.cookie-msg-close').click(function (e) {
+    e.preventDefault();
+    $('#cookie-msg').slideUp(setMessageDismissed());
+    return false;
+})
